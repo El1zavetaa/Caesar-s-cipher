71eccee807a1f23d7b685f29fa2ca8dba7983a7f
El1zavetaa <146248385+El1zavetaa@users.noreply.github.com>
2023-10-25 16:05:39 +0500

    отредактировала шифр Цезаря для русского алфавита.cpp

--- шифр Цезаря.cpp
+++ шифр Цезаря.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
-#include <string>
 #include <windows.h>
+#include <string>
 using namespace std;
 
-int main(){
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	char stroka[150], a;
-	int i, k;
-	cout << "Введите строку: ";
-	cin.getline(stroka, 150);
-	cout << "Введите шаг: ";
-	cin >> k;
-	for (i = 0; stroka[i] != '\0'; i++) {
-		if (stroka[i] != ' ' and stroka[i] != ',' and stroka[i] != '.' and stroka[i] != '!' and stroka[i] != '?' and stroka[i] != '1' and stroka[i] != '2' and stroka[i] != '3' and stroka[i] != '4' and stroka[i] != '5' and stroka[i] != '6' and stroka[i] != '7' and stroka[i] != '8' and stroka[i] != '9' and stroka[i] != '<' and stroka[i] != '>') {
-			a = stroka[i];
-			if (a >= 'a' or a <= 'z') {
-				a += k;
-				if (a > 'z') {
-					a = a - 'z' + 'a' - 1;
-				}
-				if (a < 'a') {
-					a = a + 'a' + 1;
-				}
-				stroka[i] = a;
-			}
-			else if (a >= 'A' or a <= 'Z') {
-				a += k;
-				if (a > 'Z') {
-					a = a - 'Z' + 'A' - 1;
-				}
-				if (a < 'A') {
-					a = a + 'A' + 1;
-				}
-				stroka[i] = a;
-			}
-		}
-		else
-			a = stroka[i];
-		}
-	
-	cout << "Расшифрованная строка: " << stroka;
+const int ABCSize = 33;
+
+const char low_ch[] = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к',
+ 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+const char high_ch[] = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К',
+ 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
+
+string cipher(const string& input_s, const int shift)
+{
+    bool Ok;
+    string output_s("");
+    for (unsigned i = 0; i < input_s.length(); i++)
+    {
+        Ok = false;
+        for (int j = 0; j < ABCSize; j++)
+        {
+            if (input_s[i] == low_ch[j])
+            {
+                j += shift;
+                while (j >= ABCSize) j -= ABCSize;
+                while (j < 0) j += ABCSize;
+                output_s += low_ch[j];
+                Ok = true;
+                break;
+            }
+            else if (input_s[i] == high_ch[j])
+            {
+                j += shift;
+                while (j >= ABCSize) j -= ABCSize;
+                while (j < 0) j += ABCSize;
+                output_s += high_ch[j];
+                Ok = true;
+                break;
+            }
+        }
+        if (!Ok) output_s += input_s[i];
+    }
+    return output_s;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char s[150];
+    int k;
+    cout <<"Введите строку: ";
+    cin.getline(s, 150);
+    cout << "Введите шаг: ";
+    cin >> k;
+    cout << "Зашифрованная строка: " << cipher(s, k) << ' ';
+            
 }
